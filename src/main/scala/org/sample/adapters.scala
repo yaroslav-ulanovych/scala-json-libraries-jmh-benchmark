fbd0c10005db9864ba656e5123c655840159fea5
ulanovych <yaroslav.ulanovych@gmail.com>
2015-04-18 14:23:48 +0300

    7 json libs, single test - parse sublime config to ast

---
+++ src/main/scala/org/sample/adapters.scala
@@ -0,0 +1,51 @@
+package org.sample
+
+trait Adapter {
+  def parseJsonToAst(input: String): Any
+}
+
+object Json4SNativeAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    org.json4s.native.JsonMethods.parse(input)
+  }
+}
+
+object Json4SJacksonAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    org.json4s.jackson.JsonMethods.parse(input)
+  }
+}
+
+object SprayJsonAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    import spray.json._
+    input.parseJson
+  }
+}
+
+object ScalaUtilParsingJsonAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    scala.util.parsing.json.JSON.parseRaw(input).get
+  }
+}
+
+object ArgonautAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    import argonaut._, Argonaut._
+    input.parseOption.get
+  }
+}
+
+object PlayJsonAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    import play.api.libs.json._
+    Json.parse(input)
+  }
+}
+
+object JawnAdapter extends Adapter {
+  def parseJsonToAst(input: String): Any = {
+    jawn.Parser.parseFromString(input)(jawn.ast.JawnFacade).get
+  }
+}
+
---
+++ src/main/scala/org/sample/benchmarks.scala
@@ -0,0 +1,49 @@
+package org.sample
+
+import org.openjdk.jmh.annotations.{Scope, State, Benchmark}
+
+trait BaseBenchmark {
+  def perform(adapter: Adapter): Any
+
+  @Benchmark
+  def json4sNative() = {
+    perform(Json4SNativeAdapter)
+  }
+
+  @Benchmark
+  def json4sJackson() = {
+    perform(Json4SJacksonAdapter)
+  }
+
+  @Benchmark
+  def sprayJson() = {
+    perform(SprayJsonAdapter)
+  }
+
+  @Benchmark
+  def scalaUtilParsingJson() = {
+    perform(ScalaUtilParsingJsonAdapter)
+  }
+
+  @Benchmark
+  def argonautJson() = {
+    perform(ArgonautAdapter)
+  }
+
+  @Benchmark
+  def playJson() = {
+    perform(PlayJsonAdapter)
+  }
+
+  @Benchmark
+  def jawnJson() = {
+    perform(JawnAdapter)
+  }
+}
+
+@State(Scope.Thread)
+class ParseSublimeConfig extends BaseBenchmark {
+  val input = scala.io.Source.fromInputStream(getClass.getResourceAsStream("/sublime-config.json")).mkString
+
+  def perform(adapter: Adapter) = adapter.parseJsonToAst(input)
+}
